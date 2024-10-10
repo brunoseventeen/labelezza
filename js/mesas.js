a3b54ec1a8493de8c639ff22b319c09c33cdd159
brunoseventeen <ls.bruno.ls.bruno@gmail.com>
2024-10-10 16:08:14 -0300

    subindo

--- js/mesas.js
+++ js/mesas.js
@@ -1,34 +1,44 @@
-function liberar () {
-    const mesa1 = document.getElementById('mesa1')
-    if (mesa1.classList.contains('disabled')) {
-        document.getElementById("mesasusadas").innerHTML = `<p>Mesa 1 <button onclick="lib1()">Liberar</button></p>`
+function liberar() {
+    let htmlContent = ''; // Inicializa uma string para armazenar o conteúdo HTML
+
+    for (let i = 1; i <= 25; i++) {
+        const mesa = document.getElementById(`mesa${i}`); // Captura cada mesa pelo ID
+
+        if (mesa && mesa.classList.contains('disabled')) {
+            htmlContent += `<p>Mesa ${i} <button onclick="lib(${i})">Liberar</button></p>`;
+        }
     }
-    else {
-        alert('A div não possui a classe "ativo".');
+
+    if (htmlContent) {
+        document.getElementById("mesasusadas").innerHTML = htmlContent; // Atualiza o conteúdo da div
+    } else {
+        alert('Nenhuma mesa desativada encontrada.');
     }
 }
-function lib1() {
-    const mesa1 = document.getElementById('mesa1');
+
+
+function mesa (num) {
+    const cliente = prompt('ola')
+    if (cliente !== null) {
+        localStorage.setItem('cliente',cliente)
+        alert('cliente registrado: ' + cliente + ' na mesa '+num);
+        const mesabt = document.getElementById("mesa"+num)
+        mesabt.disabled = true
+        mesabt.classList.add('disabled')
+    } else {
+        alert('Entrada cancelada.');
+    }
+}
+function lib(num) {
+    const mesa = document.getElementById('mesa'+num);
     
     // Verifica se a classe 'disable' está presente
-    if (mesa1.classList.contains('disabled')) {
-        mesa1.classList.remove('disabled'); // Remove a classe 'disable'
-        alert('Mesa 1 liberada!');
-        mesa1.disabled = false
+    if (mesa.classList.contains('disabled')) {
+        mesa.classList.remove('disabled'); // Remove a classe 'disable'
+        alert('Mesa '+num+' liberada!');
+        mesa.disabled = false
     } else {
         alert('A mesa não possui a classe "disable".');
     }
     document.getElementById("mesasusadas").innerText = ""
-}
-function mesa1 () {
-    const cliente1 = prompt('ola')
-    if (cliente1 !== null) {
-        localStorage.setItem('cliente1',cliente1)
-        alert('cliente registrado: ' + cliente1 + ' na mesa 1');
-        const mesa1bt = document.getElementById("mesa1")
-        mesa1bt.disabled = true
-        mesa1bt.classList.add('disabled')
-    } else {
-        alert('Entrada cancelada.');
-    }
 }
